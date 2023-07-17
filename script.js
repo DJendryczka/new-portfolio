fa469454c60a5598dc2b5649d86595b5552aafc8
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-07-17 11:31:47 +0200

    Added basic file structure

---
+++ script.js
@@ -0,0 +1,29 @@
+// Function showing loding page for 5 sec
+// adding display none after 5 sec
+
+
+
+const ShowLoading = setTimeout(displayLoadingPage, 1000);
+function displayLoadingPage() {
+  const para = document.createElement("p");
+  const loadNode = document.createTextNode("please wait...");
+  para.appendChild(loadNode);
+  document.getElementById("loading-page").appendChild(para);
+}
+setTimeout(loadEmulator, 4000);
+function loadEmulator() {
+  const para = document.createElement("p");
+  const loadNode = document.createTextNode("Loading JavaScript Emulator");
+  para.appendChild(loadNode);
+  setTimeout(addtextnode, 3000)
+  function addtextnode(){
+    const loadNode = document.createTextNode('....ready')
+    para.appendChild(loadNode)
+  }
+  document.getElementById("loading-page").appendChild(para);
+}
+setTimeout(hideLoadingPage, 8000);
+function hideLoadingPage() {
+  document.getElementById("loading-page").classList.add("none");
+  document.getElementById("welcome-page").classList.toggle("flex");
+}
