ed2466eda62fb7547e6b331ae1eb34ab2982f376
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-09-25 19:22:24 +0200

    Added code för displaying curent page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
-
+/* Code for displaying
+            loading page */
 
 const ShowLoading = setTimeout(displayLoadingPage, 1000);
 function displayLoadingPage() {
@@ -28,5 +29,39 @@ function hideLoadingPage() {
 }
 
 
-
+/* Code for changing active 
+            link on clicking */
+            var btns = 
+                $("#navigation .navbar-nav .nav-link");
+  
+            for (var i = 0; i < btns.length; i++) {
+                btns[i].addEventListener("click",
+                                      function () {
+                    var current = document
+                        .getElementsByClassName("active");
+  
+                    current[0].className = current[0]
+                        .className.replace(" active", "");
+  
+                    this.className += " active";
+                });
+            }
+  
+            /* Code for changing active 
+            link on Scrolling */
+            $(window).scroll(function () {
+                var distance = $(window).scrollTop();
+                $('.page-section').each(function (i) {
+  
+                    if ($(this).position().top 
+                        <= distance + 250) {
+                          
+                            $('.navbar-nav a.active')
+                                .removeClass('active');
+  
+                            $('.navbar-nav a').eq(i)
+                                .addClass('active');
+                    }
+                });
+            }).scroll();
 
